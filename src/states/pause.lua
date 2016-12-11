911532d8f2d217252bc5c4ad4017142e71078edd
Robin Hübner <profan@prfn.se>
2016-12-11 01:33:40 +0100

    also pause woo

---
+++ src/states/pause.lua
@@ -0,0 +1,38 @@
+-- shortforms
+local lg = love.graphics
+local lw = love.window
+
+local pause = Class {}
+
+-- dont give this args
+function pause:init()
+
+    self.delta = 0
+
+end
+
+function pause:update(dt)
+
+    self.delta = self.delta + dt
+
+end
+
+function pause:keypressed(key, scancode, isrepeat)
+
+    if key == "p" then
+        Gamestate.pop()
+    end
+
+end
+
+function pause:draw()
+
+    local w, h = lg.getDimensions()
+    local fsz = 12
+    local hsz = fsz * 11
+
+    lg.print("GAME IS PAUSED, PRESS P TO UNPAUSE!", w / 2 - hsz, h / 2 - fsz)
+
+end
+
+return pause
